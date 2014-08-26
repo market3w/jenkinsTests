4db5fac17e5bb836027ee6309a7ee5612124d762
fthierry <thierryflorian@free.fr>
2014-08-26 09:42:52 +0200

    commentaires

--- src/test.class.php
+++ src/test.class.php
@@ -1,11 +1,29 @@
 <?php
+/**
+ * Test Class to configure jenkins and phpdox
+ * @author Florian THIERRY
+ * @copyright 2014 Florian THIERRY
+ * @version 1.0.0
+ * @property mixed $_value
+ * @method void setValue(mixed $value)
+ * @method mixed getValue()
+ */
+ 
 class Test{
 	private $_value = "";
 	
+	/**
+	 * Set the value in $_value
+	 * @param mixed $value The value to set
+	 */
 	public function setValue($value){
 		$this->_value = $value;
 	}
 	
+	/**
+	 * Get the value of $_value
+	 * @return mixed The value you had set
+	 */
 	public function getValue(){
 		return $this->_value;
 	}
