cae2bd3f884f69b16be65502bbeb371df1d49442
fthierry <thierryflorian@free.fr>
2014-08-28 14:04:27 +0200

    test

--- src/affiche.php
+++ src/affiche.php
@@ -4,27 +4,47 @@
  * @author Florian THIERRY
  * @copyright 2014 Florian THIERRY
  * @version 1.0.0
- * @property mixed $_value
- * @method void setValue(mixed $value)
- * @method mixed getValue()
  */
  
 class Affiche{
-	private $_value = "";
-	
-	/**
-	 * Set the value in $_value
-	 * @param mixed $value The value to set
-	 */
-	public function setValue($value){
-		$this->_value = $value;
-	}
-	
-	/**
-	 * Get the value of $_value
-	 * @return mixed The value you had set
-	 */
-	public function getValue(){
-		return $this->_value;
-	}
+    
+    /**
+     * @var mixed $_value   valeur par défaut : ""
+     * @var mixed $_value2  valeur par défaut : "" 
+     */
+    
+    private $_value = "";
+    private $_value2 = "";
+
+    /**
+     * Set the value in $_value
+     * @param mixed $value The value to set
+     */
+    public function setValue($value){
+        $this->_value = $value;
+    }
+
+    /**
+     * Get the value of $_value
+     * @return mixed The value you had set
+     */
+    public function getValue(){
+        return $this->_value;
+    }
+
+    /**
+     * Set the value in $_value2
+     * @param mixed $value The value to set
+     */
+    public function setValue2($value){
+        $this->_value2 = $value;
+    }
+
+    /**
+     * Get the value of $_value2
+     * @return mixed The value you had set
+     */
+    public function getValue2(){
+        return $this->_value2;
+    }
 }
